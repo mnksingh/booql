053d7c38e16bb66e87144056d10acd80689f1faa
msfartiyal <mayank.fartiyal@affle.com>
2019-10-30 15:59:25 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = 4000;
+
+// allow cross-origin requests
+app.use(cors());
+
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose.connect(
+  "mongodb+srv://graphqluser:graphqluserpassword@cluster0-oe8dd.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+mongoose.connection.once("open", () => {
+  console.log("conneted to database");
+});
+
+// bind express with graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(PORT, () =>
+  console.log(`Server listening for requests on port ${PORT}`)
+);
